b408daedf3b8be75f3ba21f08a1568fb3da66d43
nobuti <buti@nobuti.com>
2023-01-30 07:54:36 +0100

    feat: assign user management

--- client/src/app/views/ticket/index.tsx
+++ client/src/app/views/ticket/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, ChangeEvent } from 'react';
+import { useCallback, useMemo, ChangeEvent } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Navigate, useParams } from 'react-router-dom';
 import { Ticket, User } from '@acme/shared-models';
@@ -6,6 +6,8 @@ import { Ticket, User } from '@acme/shared-models';
 import getTicket from '../../../context/tickets/compose/get';
 import completeTicket from '../../../context/tickets/compose/complete';
 import uncompleteTicket from '../../../context/tickets/compose/uncomplete';
+import assignTicket from '../../../context/tickets/compose/assign';
+import unassignTicket from '../../../context/tickets/compose/unassign';
 import getAllUsers from '../../../context/users/compose/getAll';
 import Switch from '../../components/Switch';
 
@@ -44,6 +46,39 @@ const TicketDetails = () => {
     },
   });
 
+  const assignMutation = useMutation({
+    mutationFn: (user: number) => assignTicket(ticket as Ticket, user),
+    onSettled: () => {
+      queryClient.invalidateQueries(['tickets']);
+      queryClient.invalidateQueries(['ticket', id]);
+    },
+  });
+
+  const unassignMutation = useMutation({
+    mutationFn: unassignTicket,
+    onSettled: () => {
+      queryClient.invalidateQueries(['tickets']);
+      queryClient.invalidateQueries(['ticket', id]);
+    },
+  });
+
+  const onAssignHandle = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      if (assignMutation.isLoading || unassignMutation.isLoading) {
+        return;
+      }
+      const assigneeId = e.target.value ? Number(e.target.value) : undefined;
+      console.log(typeof e.target.value, assigneeId);
+
+      if (ticket) {
+        assigneeId
+          ? assignMutation.mutate(assigneeId)
+          : unassignMutation.mutate(ticket);
+      }
+    },
+    [ticket, assignMutation, unassignMutation]
+  );
+
   const onCompleteHandle = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
       if (uncompleteMutation.isLoading || completeMutation.isLoading) {
@@ -59,6 +94,25 @@ const TicketDetails = () => {
     [ticket, completeMutation, uncompleteMutation]
   );
 
+  const userOptions = useMemo(
+    () =>
+      users.map((u) => (
+        <option key={u.id} value={u.id}>
+          {u.name}
+        </option>
+      )),
+    [users]
+  );
+
+  const selectedValue = useMemo(() => {
+    if (ticket) {
+      return ticket.assigneeId
+        ? users.find((u) => u.id === ticket.assigneeId)?.id
+        : '';
+    }
+    return undefined;
+  }, [ticket, users]);
+
   if (status === 'loading') return <div>Loading...</div>;
   if (status === 'error') return <Navigate to="/404" replace />;
 
@@ -68,9 +122,14 @@ const TicketDetails = () => {
       <dl>
         <dt>Asignee:</dt>
         <dd>
-          {ticket.assigneeId
-            ? users.find((u) => u.id === ticket.assigneeId)?.name
-            : 'Unassigned'}
+          <select
+            value={selectedValue}
+            onChange={onAssignHandle}
+            disabled={assignMutation.isLoading || unassignMutation.isLoading}
+          >
+            <option value={''}>Unassigned</option>
+            {userOptions}
+          </select>
         </dd>
         <dt>Completed:</dt>
         <dd>
